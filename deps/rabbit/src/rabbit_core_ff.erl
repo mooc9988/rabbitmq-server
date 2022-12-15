90c8d0e0773ba84f2c4a0177f808f077cdf0ff68
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2022-12-15 11:34:44 +0100

    rabbit_feature_flags: Mark all 3.11.x feature flags as required
    The main reason is that we want to have no migration functions at this
    point to make it easier to handle the Mnesia/Khepri migration.
    
    While here, mark all feature flags as required even those without a
    migration function to start from a clean slate after this. End users
    will have to upgrade to RabbitMQ 3.11.x or 3.12.x first before they can
    upgrade to whatever follows 3.12.x anyway.

--- deps/rabbit/src/rabbit_core_ff.erl
+++ deps/rabbit/src/rabbit_core_ff.erl
@@ -7,16 +7,10 @@
 
 -module(rabbit_core_ff).
 
--export([direct_exchange_routing_v2_enable/1,
-         listener_records_in_ets_enable/1,
-         listener_records_in_ets_post_enable/1,
-         tracking_records_in_ets_enable/1,
-         tracking_records_in_ets_post_enable/1]).
-
 -rabbit_feature_flag(
    {classic_mirrored_queue_version,
     #{desc          => "Support setting version for classic mirrored queues",
-      stability     => stable
+      stability     => required
      }}).
 
 -rabbit_feature_flag(
@@ -30,7 +24,7 @@
    {stream_queue,
     #{desc          => "Support queues of type `stream`",
       doc_url       => "https://www.rabbitmq.com/stream.html",
-      stability     => stable,
+      stability     => required,
       depends_on    => [quorum_queue]
      }}).
 
@@ -63,7 +57,7 @@
    {stream_single_active_consumer,
     #{desc          => "Single active consumer for streams",
       doc_url       => "https://www.rabbitmq.com/stream.html",
-      stability     => stable,
+      stability     => required,
       depends_on    => [stream_queue]
      }}).
 
@@ -76,38 +70,29 @@
 -rabbit_feature_flag(
    {direct_exchange_routing_v2,
     #{desc       => "v2 direct exchange routing implementation",
-      stability  => stable,
-      depends_on => [feature_flags_v2, implicit_default_bindings],
-      callbacks  => #{enable => {?MODULE, direct_exchange_routing_v2_enable}}
+      stability  => required,
+      depends_on => [feature_flags_v2, implicit_default_bindings]
      }}).
 
 -rabbit_feature_flag(
    {listener_records_in_ets,
     #{desc       => "Store listener records in ETS instead of Mnesia",
-      stability  => stable,
-      depends_on => [feature_flags_v2],
-      callbacks  => #{enable =>
-                      {?MODULE, listener_records_in_ets_enable},
-                      post_enable =>
-                      {?MODULE, listener_records_in_ets_post_enable}}
+      stability  => required,
+      depends_on => [feature_flags_v2]
      }}).
 
 -rabbit_feature_flag(
    {tracking_records_in_ets,
     #{desc          => "Store tracking records in ETS instead of Mnesia",
-      stability     => stable,
-      depends_on    => [feature_flags_v2],
-      callbacks     => #{enable =>
-                             {?MODULE, tracking_records_in_ets_enable},
-                         post_enable =>
-                             {?MODULE, tracking_records_in_ets_post_enable}}
+      stability     => required,
+      depends_on    => [feature_flags_v2]
      }}).
 
 -rabbit_feature_flag(
    {classic_queue_type_delivery_support,
     #{desc          => "Bug fix for classic queue deliveries using mixed versions",
       doc_url       => "https://github.com/rabbitmq/rabbitmq-server/issues/5931",
-      stability     => stable,
+      stability     => required,
       depends_on    => [stream_queue]
      }}).
 
@@ -118,120 +103,3 @@
       stability     => stable,
       depends_on    => [stream_queue]
      }}).
-
-%% -------------------------------------------------------------------
-%% Direct exchange routing v2.
-%% -------------------------------------------------------------------
-
--spec direct_exchange_routing_v2_enable(Args) -> Ret when
-      Args :: rabbit_feature_flags:enable_callback_args(),
-      Ret :: rabbit_feature_flags:enable_callback_ret().
-direct_exchange_routing_v2_enable(#{feature_name := FeatureName}) ->
-    TableName = rabbit_index_route,
-    ok = rabbit_table:wait([rabbit_route], _Retry = true),
-    try
-        ok = rabbit_table:create(
-               TableName, rabbit_table:rabbit_index_route_definition()),
-        case rabbit_table:ensure_table_copy(TableName, node(), ram_copies) of
-            ok ->
-                ok = rabbit_binding:populate_index_route_table();
-            {error, Err} = Error ->
-                rabbit_log_feature_flags:error(
-                  "Feature flags: `~ts`: failed to add copy of table ~ts to "
-                  "node ~tp: ~tp",
-                  [FeatureName, TableName, node(), Err]),
-                Error
-        end
-    catch throw:{error, Reason} ->
-              rabbit_log_feature_flags:error(
-                "Feature flags: `~ts`: enable callback failure: ~tp",
-                [FeatureName, Reason]),
-              {error, Reason}
-    end.
-
-%% -------------------------------------------------------------------
-%% Listener records moved from Mnesia to ETS.
-%% -------------------------------------------------------------------
-
-listener_records_in_ets_enable(#{feature_name := FeatureName}) ->
-    try
-        rabbit_misc:execute_mnesia_transaction(
-          fun () ->
-                  mnesia:lock({table, rabbit_listener}, read),
-                  Listeners = mnesia:select(
-                                rabbit_listener, [{'$1',[],['$1']}]),
-                  lists:foreach(
-                    fun(Listener) ->
-                            ets:insert(rabbit_listener_ets, Listener)
-                    end, Listeners)
-          end)
-    catch
-        throw:{error, {no_exists, rabbit_listener}} ->
-            ok;
-        throw:{error, Reason} ->
-            rabbit_log_feature_flags:error(
-              "Feature flags: `~ts`: failed to migrate Mnesia table: ~tp",
-              [FeatureName, Reason]),
-            {error, Reason}
-    end.
-
-listener_records_in_ets_post_enable(#{feature_name := FeatureName}) ->
-    try
-        case mnesia:delete_table(rabbit_listener) of
-            {atomic, ok} ->
-                ok;
-            {aborted, {no_exists, _}} ->
-                ok;
-            {aborted, Err} ->
-                rabbit_log_feature_flags:error(
-                  "Feature flags: `~ts`: failed to delete Mnesia table: ~tp",
-                  [FeatureName, Err]),
-                ok
-        end
-    catch
-        throw:{error, Reason} ->
-            rabbit_log_feature_flags:error(
-              "Feature flags: `~ts`: failed to delete Mnesia table: ~tp",
-              [FeatureName, Reason]),
-            ok
-    end.
-
-tracking_records_in_ets_enable(#{feature_name := FeatureName}) ->
-    try
-        rabbit_connection_tracking:migrate_tracking_records(),
-        rabbit_channel_tracking:migrate_tracking_records()
-    catch
-        throw:{error, {no_exists, _}} ->
-            ok;
-        throw:{error, Reason} ->
-            rabbit_log_feature_flags:error("Enabling feature flag ~ts failed: ~tp",
-                                           [FeatureName, Reason]),
-            {error, Reason}
-    end.
-
-tracking_records_in_ets_post_enable(#{feature_name := FeatureName}) ->
-    try
-        [delete_table(FeatureName, Tab) ||
-            Tab <- rabbit_connection_tracking:get_all_tracked_connection_table_names_for_node(node())],
-        [delete_table(FeatureName, Tab) ||
-            Tab <- rabbit_channel_tracking:get_all_tracked_channel_table_names_for_node(node())]
-    catch
-        throw:{error, Reason} ->
-            rabbit_log_feature_flags:error("Enabling feature flag ~ts failed: ~tp",
-                                           [FeatureName, Reason]),
-            %% adheres to the callback interface
-            ok
-    end.
-
-delete_table(FeatureName, Tab) ->
-    case mnesia:delete_table(Tab) of
-        {atomic, ok} ->
-            ok;
-        {aborted, {no_exists, _}} ->
-            ok;
-        {aborted, Err} ->
-            rabbit_log_feature_flags:error("Enabling feature flag ~ts failed to delete mnesia table ~tp: ~tp",
-                                           [FeatureName, Tab, Err]),
-            %% adheres to the callback interface
-            ok
-    end.
